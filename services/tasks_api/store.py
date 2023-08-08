faa419cd579446ad861c4b15515cb45f439297a4
Mesele Ayele <mayele@synergemtech.com>
2023-08-08 10:34:13 -0400

    fix test failure

--- services/tasks_api/store.py
+++ services/tasks_api/store.py
@@ -18,6 +18,8 @@ class TaskStore:
             Item={
                 "PK": f"#{task.owner}",
                 "SK": f"#{task.id}",
+                "GS1PK": f"#{task.owner}#{task.status.value}",
+                "GS1SK": f"#{datetime.datetime.utcnow().isoformat()}",
                 "id": str(task.id),
                 "title": task.title,
                 "status": task.status.value,
@@ -26,7 +28,7 @@ class TaskStore:
         )
 
     def get_by_id(self, task_id, owner):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)  # new
         table = dynamodb.Table(self.table_name)
         record = table.get_item(
             Key={
@@ -40,54 +42,6 @@ class TaskStore:
             owner=record["Item"]["owner"],
             status=TaskStatus[record["Item"]["status"]],
         )
-
-    def add(self, task):
-        dynamodb = boto3.resource("dynamodb")
-        table = dynamodb.Table(self.table_name)
-        table.put_item(
-            Item={
-                "PK": f"#{task.owner}",
-                "SK": f"#{task.id}",
-                "GS1PK": f"#{task.owner}#{task.status.value}",
-                "GS1SK": f"#{datetime.datetime.utcnow().isoformat()}",
-                "id": str(task.id),
-                "title": task.title,
-                "status": task.status.value,
-                "owner": task.owner,
-            }
-        )
-
-    def list_open(self, owner):
-        dynamodb = boto3.resource("dynamodb")
-        table = dynamodb.Table(self.table_name)
-        last_key = None
-        query_kwargs = {
-            "IndexName": "GS1",
-            "KeyConditionExpression": Key("GS1PK").eq(
-                f"#{owner}#{TaskStatus.OPEN.value}"
-            ),
-        }
-        tasks = []
-        while True:
-            if last_key is not None:
-                query_kwargs["ExclusiveStartKey"] = last_key
-            response = table.query(**query_kwargs)
-            tasks.extend(
-                [
-                    Task(
-                        id=UUID(record["id"]),
-                        title=record["title"],
-                        owner=record["owner"],
-                        status=TaskStatus[record["status"]],
-                    )
-                    for record in response["Items"]
-                ]
-            )
-            last_key = response.get("LastEvaluatedKey")
-            if last_key is None:
-                break
-
-        return tasks
 
     def list_open(self, owner):
         return self._list_by_status(owner, TaskStatus.OPEN)
